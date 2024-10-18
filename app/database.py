11687078d96be3a0135672610aabe7224907bafd
lafonsecallorca <127278198+lafonsecallorca@users.noreply.github.com>
2024-10-18 14:03:44 -0400

    adding docstring for module and method

--- app/database.py
+++ app/database.py
@@ -1,4 +1,19 @@
-# database.py
+"""
+database.py
+
+This module sets up the connection to the database using SQLAlchemy.
+It configures the database engine, creates a session factory for handling
+database transactions, and defines a base class for all database models.
+
+The `get_db` function is used to provide a session for each request and 
+ensure the session is closed after the request is processed.
+
+Dependencies:
+- SQLAlchemy
+- sessionmaker
+- declarative_base
+"""
+
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -26,6 +41,18 @@ Base = declarative_base()
 
 # Dependency for database session
 def get_db():
+    """
+    Provide a new database session for each request.
+
+    This function is a generator that yields a database session and ensures
+    the session is properly closed after the request is processed. It is 
+    typically used as a dependency in FastAPI routes to handle database 
+    transactions.
+
+    Yields:
+        db (Session): A SQLAlchemy session that can be used to interact with 
+        the database.
+    """
     db = SessionLocal()
     try:
         yield db
